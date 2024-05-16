0caee33db5a934d66b67712c9c479375221cf8fb
Ayushmaan <76957103+j1ntan@users.noreply.github.com>
2024-05-16 12:23:35 +0530

    made some adjustments

--- instimap/locations/views.py
+++ instimap/locations/views.py
@@ -3,11 +3,122 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from locations.serializers import LocationSerializer
 from locations.models import Location
-from roles.helpers import insti_permission_required
-from roles.helpers import login_required_ajax
-from roles.helpers import user_has_insti_privilege
-from roles.helpers import user_has_privilege
-from roles.helpers import forbidden_no_privileges
+
+from locations.models import UserProfile, SSOBan, Body
+import datetime
+from dateutil.relativedelta import relativedelta
+
+#from roles.helpers import insti_permission_required
+
+def insti_permission_required(permission):
+    """Check for institute permission."""
+
+    def real_decorator(func):
+        @login_required_ajax
+        @add_doc(func.__doc__)
+        def wrapper(*args, **kw):
+            if user_has_insti_privilege(args[1].user.profile, permission):
+                return func(*args, **kw)
+            return forbidden_no_privileges()
+
+        return wrapper
+
+    return real_decorator
+
+#from roles.helpers import login_required_ajax
+
+def add_doc(value):
+    def _doc(func):
+        func.__doc__ = value
+        return func
+
+    return _doc
+
+def user_is_banned(profile):
+    try:
+        ban = SSOBan.objects.get(banned_user=profile.id)
+        current_time = datetime.datetime.now()
+        ban_created = ban.time_of_creation
+        ban_duration = ban.duration_of_ban
+
+        if ban_duration == "Permanent":
+            return True
+
+        else:
+            duration_month = int(ban_duration.split(" ")[0])
+            banned_till = ban_created + relativedelta(months=duration_month)
+
+            if banned_till > current_time:
+                return True
+        return False
+    except SSOBan.DoesNotExist:
+        return False
+
+def login_required_ajax(func):
+    """
+    Just make sure the user is authenticated to access a certain ajax view
+
+    Otherwise return a HttpResponse 401 - authentication required
+    instead of the 302 redirect of the original Django decorator
+    """
+
+    @add_doc(func.__doc__)
+    def wrapper(*args, **kw):
+        if args[1].user.is_authenticated:
+            user = args[1].user
+            profile = UserProfile.objects.get(user=user)
+            if not user_is_banned(profile):
+                return func(*args, **kw)
+            if user_is_banned:
+                return Response(
+                    {"message": "banned", "detail": "your SSO has been banned/disabled"}
+                )
+        return Response(
+            {"message": "unauthenticated", "detail": "Log in to continue!"}, status=401
+        )
+
+    return wrapper
+
+#from roles.helpers import user_has_insti_privilege
+
+def check_roles(roles, privilege):
+    """Private helper function."""
+    return any([(privilege in role.permissions) for role in roles])
+
+def user_has_insti_privilege(profile, privilege):
+    """Returns true if UserProfile has the institute privilege."""
+    return check_roles(profile.institute_roles.all(), privilege)
+
+#from roles.helpers import user_has_privilege
+
+def get_parents_recursive(body, parents):
+    """Get array of parents. Includes the body itself."""
+    for child_body_relation in body.parents.all():
+        get_parents_recursive(child_body_relation.parent, parents)
+    parents.append(body)
+    return parents
+
+def user_has_privilege(profile, bodyid, privilege):
+    """Returns true if UserProfile has or has inherited the privilege."""
+    body = Body.objects.get(pk=bodyid)
+    parents = get_parents_recursive(body, [])
+    for role in profile.roles.all().filter(body__in=parents):
+        if (role.body == body or role.inheritable) and privilege in role.permissions:
+            return True
+    return False
+
+#from roles.helpers import forbidden_no_privileges
+
+def forbidden_no_privileges():
+    """Forbidden due to insufficient privileges."""
+    return Response(
+        {
+            "message": "insufficient privileges",
+            "detail": "You have insufficient priveleges to perform this action.",
+        },
+        status=403,
+    )
+
 from django.db.models import Q
 from django.http import HttpRequest
 import sys
