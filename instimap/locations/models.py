71370c0e66f4b4f65a8c3a502d637075a9a09af9
Manish <msn07082005@gmail.com>
2024-06-13 07:39:08 +0530

    Add : UserProfile model

--- instimap/locations/models.py
+++ instimap/locations/models.py
@@ -2,6 +2,8 @@
 from uuid import uuid4
 from django.db import models
 from locations.management.commands.adj_updater import UpdateAdjList
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 def get_url_friendly(name):
     """Converts the name to a url friendly string for use in `str_id`"""
@@ -223,3 +225,88 @@ class LocationLocationDistance(models.Model):
     class Meta:
         verbose_name = "Location-Location Distance"
         verbose_name_plural = "Location-Location Distances"
+
+class UserProfile(models.Model):
+    """Profile of a unique user."""
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    last_ping = models.DateTimeField(default=now)
+
+    # Linked Django User object
+    user = models.OneToOneField(
+        User, related_name="profile", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    # Basic info from SSO
+    name = models.CharField(max_length=50, blank=True)
+    roll_no = models.CharField(max_length=30, null=True, blank=True)
+    ldap_id = models.CharField(max_length=50, null=True, blank=True)
+    profile_pic = models.URLField(null=True, blank=True)
+
+    # Advanced info from SSO
+    contact_no = models.CharField(max_length=30, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    department = models.CharField(max_length=30, null=True, blank=True)
+    department_name = models.CharField(max_length=200, null=True, blank=True)
+    degree = models.CharField(max_length=200, null=True, blank=True)
+    degree_name = models.CharField(max_length=200, null=True, blank=True)
+    join_year = models.CharField(max_length=5, null=True, blank=True)
+    graduation_year = models.CharField(max_length=5, null=True, blank=True)
+    hostel = models.CharField(max_length=100, null=True, blank=True)
+    room = models.CharField(max_length=30, null=True, blank=True)
+
+    # InstiApp feature fields
+    # active = models.BooleanField(default=True)
+    # followed_bodies = models.ManyToManyField(
+    #     "bodies.Body", related_name="followers", blank=True
+    # )
+    # InstiApp roles
+    # roles = models.ManyToManyField("roles.BodyRole", related_name="users", blank=True)
+    # former_roles = models.ManyToManyField(
+    #     "roles.BodyRole",
+    #     related_name="former_users",
+    #     blank=True,
+    #     through="UserFormerRole",
+    # )
+    # institute_roles = models.ManyToManyField(
+    #     "roles.InstituteRole", related_name="users", blank=True
+    # )
+    # community_roles = models.ManyToManyField('roles.CommunityRole', related_name='users', blank=True)
+    # User exposed fields
+    show_contact_no = models.BooleanField(default=False)
+    fcm_id = models.CharField(max_length=200, null=True, blank=True)
+    about = models.TextField(blank=True, null=True)
+    android_version = models.IntegerField(default=0)
+    website_url = models.URLField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    
+    can_create_location = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+        indexes = [
+            models.Index(
+                fields=[
+                    "active",
+                ]
+            ),
+            models.Index(
+                fields=[
+                    "ldap_id",
+                ]
+            ),
+        ]
+
+    class ExMeta:
+        user_editable = (
+            "show_contact_no",
+            "fcm_id",
+            "about",
+            "android_version",
+            "website_url",
+        )
+
+    def __str__(self):
+        return f"{self.name} - {self.roll_no}"
+
